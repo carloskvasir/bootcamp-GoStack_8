24c4b360c533322f9fdfce39b135be53410b8c8f
Carlos Lima (kvasir) <carloskvasir@gmail.com>
2019-10-16 13:46:55 -0300

    10 - CRUD

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,16 +2,47 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = { "name": "Diego", "email": "aa@bb.cc" }
 
 const users = ['Diego', 'Cláudio', 'Carlos'];
 
+server.get('/users', (req, res) => {
+  return res.json(users);
+})
+
 server.get('/users/:index', (req, res) => {
   const { index } = req.params;
 
   return res.json(users[index]);
+})
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+})
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const newName = req.body.name;
+
+  users[index] = newName;
+
+  return res.json(users);
+})
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
 })
 
 server.listen(3000);
