8fefac416e4443872f40dccee14d8a734b1801ce
Carlos Lima (kvasir) <carloskvasir@gmail.com>
2019-10-16 12:40:07 -0300

    fix body text

--- modulo01/index.js
+++ modulo01/index.js
@@ -4,7 +4,7 @@ const server = express();
 
 // Query params = ?teste=1
 // Route params = /users/1
-// Request boy = { "name": "Diego", "email": "aa@bb.cc" }
+// Request body = { "name": "Diego", "email": "aa@bb.cc" }
 
 server.get('/users/:id', (req, res) => {
   const { id } = req.params;
