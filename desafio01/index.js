7a18896578d5287a670e482bf01274d9af3a03ba
Carlos Lima (kvasir) <carloskvasir@gmail.com>
2019-10-16 23:52:13 -0300

    parte do desafio

--- desafio01/index.js
+++ desafio01/index.js
@@ -1,11 +1,42 @@
 const express = require("express");
 
 const server = express();
+server.use(express.json());
+
+const projects = [];
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const newProject = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(newProject);
+
+  return res.json(projects);
+});
+
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(projects);
+});
+
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
 
 
-server.get('/test', (req, res) => {
-  res.send('Hello Word');
-})
-
-
+});
 server.listen(3000);
