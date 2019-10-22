0167dae98e8fc22cb2ecc9d30c3a963ee1c2ab32
Carlos Lima (kvasir) <carloskvasir@gmail.com>
2019-10-22 00:56:01 -0200

    colocando configurações adicionais

--- gobarber/.eslintrc.js
+++ gobarber/.eslintrc.js
@@ -15,5 +15,13 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    // todos os metodos da clase usam this
+    "class-methods-use-this": "off",
+    // não pode manipular parametros recebidos
+    "no-param-ressign": "off",
+    // use apenas camelCase
+    "camelcase":"off",
+    // Não de erro quando não utilizar a var next do express
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
   },
 };
