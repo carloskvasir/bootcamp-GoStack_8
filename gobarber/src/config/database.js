25f5123e9070f2b03d226b7db2f896d4a6fd8e9a
Carlos Lima (kvasir) <carloskvasir@gmail.com>
2019-10-24 11:59:14 -0200

    fix plus s

--- gobarber/src/config/database.js
+++ gobarber/src/config/database.js
@@ -4,7 +4,7 @@ module.exports = {
   username: 'postgres',
   password: 'docker',
   database: 'gobarber',
-  defines: {
+  define: {
     timestamps: true,
     underscorded: true,
     underscordedAll: true,
