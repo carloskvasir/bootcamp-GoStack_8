778f0af7a2698901611430554492ad6f605b81d1
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2019-12-28 11:36:34 -0300

    upload using multer

---
+++ gobarber/src/config/multer.js
@@ -0,0 +1,17 @@
+import multer from 'multer';
+import crypto from 'crypto';
+import { extname, resolve } from 'path';
+
+export default {
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, res) => {
+        if (err) return cb(err);
+
+        // 0123456789abcdef.png
+        return cb(null, res.toString('hex') + extname(file.originalname));
+      });
+    },
+  }),
+};
