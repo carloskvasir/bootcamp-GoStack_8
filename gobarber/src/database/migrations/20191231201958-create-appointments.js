0b071af312c8b3ae3b119ce79f2b77c3c7da9a6b
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2019-12-31 17:29:47 -0300

    create appointments in DB

--- gobarber/src/database/migrations/20191231201958-create-appointments.js
+++ gobarber/src/database/migrations/20191231201958-create-appointments.js
@@ -7,6 +7,27 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
       },
+      date: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
