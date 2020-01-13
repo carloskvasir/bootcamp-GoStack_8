93d7e1a2fc5373790f806455b283730619860052
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2020-01-13 20:48:03 -0300

    16 - Configurando Nodemailer

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -2,6 +2,7 @@ import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import ptbr from 'date-fns/locale/pt-BR';
 import * as Yup from 'yup';
 
+import Mail from '../../lib/Mail';
 import Appointment from '../models/Appointment';
 import File from '../models/File';
 import User from '../models/User';
@@ -108,7 +109,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -126,6 +135,16 @@ class AppointmentController {
 
     appointment.canceled_at = new Date();
     await appointment.save();
+
+    console.log(`${appointment.provider.name} <${appointment.provider.email}>`);
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Vôce tem um novo cancelamento.',
+    })
+      .then(success => console.log('success: ', success))
+      .catch(error => console.log('error: ', error));
 
     return res.json(appointment);
   }
